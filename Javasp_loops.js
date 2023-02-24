b719ff88cc20e9b004ea9456384003a70b2d27bd
nkatajoshua <nkatajoshua4@gmail.com>
2023-02-24 17:18:09 +0300

    Create Javasp_loops.js

---
+++ Javasp_loops.js
@@ -0,0 +1,20 @@
+//do while loop
+let fname = "Joshua";
+do{
+    console.log("Yes,his name is"+fname);
+} while(fname !="Joshua");
+
+//while loop
+let n = 0;
+while (n <= 5){
+    console.log("This is while loop number"+n);
+    n++;
+}
+
+//for in loop
+let Lnames = ["Joe","Tate","Tom","Timo"]
+for (let m in Lnames){
+    console.log(Lnames[m]);
+}
+
+//for of loop
